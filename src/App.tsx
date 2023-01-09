80e6314ee1c96360a01a9753e04174d31b18ff9e
Evgeny Vodyannikov <vodyannikov.eugene@gmail.com>
2023-01-09 22:13:56 +0500

    import bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navigation from './components/Navigation'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
 
